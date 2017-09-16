3008b7d1c6b9c28381b2eee85dd1e63128f079e1
conanluffy <923561179@qq.com>
2017-09-16 23:37:33 +0800

    9/16 update

--- menu.js
+++ menu.js
@@ -1,7 +1,7 @@
 var menu = [
     ['单页面制作',
         ['demo/vigenere.html','维吉尼亚密码'],
-        ['demo/warriors.html','NBA金州勇士队数据统计'],
+        // ['demo/warriors.html','NBA金州勇士队数据统计'],
         ['demo/ife/task_1_01_1.html', 'HTML页面'],
         ['demo/ife/task_1_03_1.html', '三栏式布局'],
         ['components/csscenter/app/index.html', '定位及居中'],
@@ -9,15 +9,15 @@ var menu = [
         ['demo/ife/task_1_10_1.html', 'Flexbox布局'],
         ['demo/ife/task_1_12_1.html', '一些CSS3新特性'],
         ['ife/task_1_7/task_1_7.html', '常见的技术产品官网的页面架构及样式布局'],
-        ['demo/component/301/svg.html', 'SVG 初试'],
-        ['demo/ife/task_1_02_1.html', 'HTML+CSS页面Ⅰ'],
-        ['demo/ife/task_1_05_1.html', 'HTML+CSS页面Ⅱ'],
+        // ['demo/component/301/svg.html', 'SVG 初试'],
+        // ['demo/ife/task_1_02_1.html', 'HTML+CSS页面Ⅰ'],
+        // ['demo/ife/task_1_05_1.html', 'HTML+CSS页面Ⅱ'],
         ['demo/pages/bootstrap/index.html', 'Bootstrap 初试'],
         ['demo/ife/task_1_11_1.html', '自适应移动Web页面'],
-        ['demo/ife/task_1_06_1.html', '模拟报纸排版'],
+        // ['demo/ife/task_1_06_1.html', '模拟报纸排版'],
         ['demo/ife/task_1_09_1.html', '一个复杂页面'],
         ['demo/pages/eleme/index.html', '仿饿了么首页'],
-        ['components/somecss3/app/index.html','Try CSS3']
+        // ['components/somecss3/app/index.html','Try CSS3']
     ],
     ['网页小部件',
         ['demo/component/101/index.html', '简易电子时钟'],
@@ -30,21 +30,21 @@ var menu = [
         ['demo/algorithms/sort.html', '排序可视化'],
         ['demo/ife/task_3_37_1.html', '浮出层'],
         ['demo/component/111/index.html', '滚动条'],
-        ['components/calendar/demo.html', '日历组件'],
+        // ['components/calendar/demo.html', '日历组件'],
         ['demo/component/401/index.html', '滑动轮播图'],
         ['components/fadeslide/index.html', '渐显渐隐轮播图']
     ],
-    ['小项目',
-        ['demo/cnpolice/pages/login.html','长宁区实有人口']
-    ],
+    // ['小项目',
+    //     ['demo/cnpolice/pages/login.html','长宁区实有人口']
+    // ],
     // ['Vue 相关', 
     //     ['components/cssfilter/app/index.html', 'Filter生成器']
     // ],
-    ['Node 相关', 
-        // ['https://github.com/conanluffy/conanluffy.github.io', '一个博客'],
-        ['fe/node/spider/001/index.html','001'],
-        ['fe/node/spider/002/index.html','002'],
-    ]
+    // ['Node 相关', 
+    //     // ['https://github.com/conanluffy/conanluffy.github.io', '一个博客'],
+    //     ['fe/node/spider/001/index.html','001'],
+    //     ['fe/node/spider/002/index.html','002'],
+    // ]
 ];
 var list = "";
 for (var i = 0; i < menu.length; i++) {
