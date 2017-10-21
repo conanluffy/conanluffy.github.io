8c64454689f8ee07cf2ef80a74bc8089296ec39e
conanluffy <923561179@qq.com>
2017-10-21 14:18:01 +0800

    analytics tool change

---
+++ js/baidu-analytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ac7b72af6deb9935a12f505013fba83d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
