fc546ca9f147c2386f5ddfee7fc92d5a9fd7f924
Qingcheng He <923561179@qq.com>
2018-03-16 18:37:09 +0800

    Update NO.2.js

--- demo/wallet-guard/js/NO.2.js
+++ demo/wallet-guard/js/NO.2.js
@@ -1,16 +1,35 @@
 (function() {
-	var ie6 = document.all;
-	var dv = document.getElementById('navigator'), st;
-	dv.attr('otop', dv.offset().top); //存储原来的距离顶部的距离
-	$(window).scroll(function () {
-		st = Math.max(document.body.scrollTop || document.documentElement.scrollTop);
-		if (st > parseInt(dv.attr('otop'))) { 
-			if (dv.css('position') != 'fixed') {
-				dv.css({'position': 'fixed', top: 0});
-			}
-		}
-		else if (dv.css('position') != 'static'){
-			dv.css({'position': 'static'});
-		}
-	});
+  var ie6 = document.all;
+  var dv = document.getElementById('navigator'), st;
+  function getOffset(node, offset) {
+    // 创建一个对象存储offset
+    if (!offset) {
+      offset = {};
+      offset.top = 0;
+      offset.left = 0;
+    }
+    
+    // 当该节点为body节点时，结束递归
+    if (node == document.body) {
+      return offset;
+    }
+    
+    offset.top += node.offsetTop;
+    offset.left += node.offsetLeft;
+    
+    // 向父元素累加offset里的值
+    return getOffset(node.parentNode, offset);
+  }
+  dv.setAttribute('otop', getOffset(dv).top); //存储原来的距离顶部的距离
+  $(window).scroll(function () {
+    st = Math.max(document.body.scrollTop || document.documentElement.scrollTop);
+    if (st > parseInt(dv.getAttribute('otop'))) { 
+      if (dv.style.position != 'fixed') {
+        dv.setAttribute('style','position: fixed;top: 0;');
+      }
+    }
+    else if (dv.style.position != 'static'){
+      dv.setAttribute('style','position: static;');
+    }
+  });
 })();
